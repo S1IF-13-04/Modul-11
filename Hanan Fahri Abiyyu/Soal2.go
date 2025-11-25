82ad20098973fb81ba80161546aa38a1f5ad23f3
Hanan Fahri Abiyyu <hananfahri78@gmail.com>
2025-11-25 22:16:39 +0700

    Create Soal2.go

---
+++ Hanan Fahri Abiyyu/Soal2.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kendaraan string
+	var durasi int
+	fmt.Scan(&kendaraan, &durasi)
+	motor := durasi * 2000
+	mobil := durasi * 5000
+	truk := durasi * 8000
+	
+	switch kendaraan {
+	case "motor":
+		fmt.Println("Rp", motor)
+	case "mobil" :
+		fmt.Println("Rp", mobil)
+	case "truk" :
+		fmt.Println("Rp", truk)
+	}
+}
