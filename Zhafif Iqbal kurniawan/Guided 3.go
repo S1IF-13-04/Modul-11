196b799633ecd95da55b22e0c4140761a457463c
Zhafif Iqbal Kurniawan <zhafif.iqbal.123@gmail.com>
2025-11-26 23:34:12 +0700

    Add files via upload

---
+++ Zhafif Iqbal kurniawan/Guided 3.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+func main() {
+	var kendaraan string
+	var durasi int
+	fmt.Print("Masukkan jenis kendaraan (Motor/mobil/truk) : ")
+	fmt.Scan(&kendaraan)
+	fmt.Print("Masukkan durasi parkir (dalam jam) : ")
+	fmt.Scan(&durasi)
+
+	switch kendaraan {
+	case "Motor":
+		if durasi >= 1 && durasi <= 2 {
+			fmt.Println("Tarif parkir : Rp 7000")
+		} else if durasi > 2 {
+			fmt.Println("Tarif parkir : Rp 9000")
+		}
+	case "Mobil":
+		if durasi >= 1 && durasi <= 2 {
+			fmt.Println("Tarif parkir : Rp 15.000")
+		} else if durasi > 2 {
+			fmt.Println("Tarif parkir : Rp 20.000")
+		}
+	case "Truk":
+		if durasi >= 1 && durasi <= 2 {
+			fmt.Println("Tarif parkir : Rp 25.000")
+		} else if durasi > 2 {
+			fmt.Println("Tarif parkir : Rp 35.000")
+		}
+	default:
+		fmt.Println("Jenis kendaraan atau durasi parkir tidak valid \nTarif parkir : Rp 0")
+	}
+}
---
+++ Zhafif Iqbal kurniawan/Latihan 3.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bilangan int
+	fmt.Scan(&bilangan)
+
+	switch {
+	case bilangan%10 == 0:
+		fmt.Println("Kategori: Bilangan Kelipatan 10")
+		fmt.Printf("Hasil pembagian antara %d / 10 = %d\n", bilangan, bilangan/10)
+	case bilangan%5 == 0 && bilangan != 5:
+		fmt.Println("Kategori: Bilangan Kelipatan 5")
+		fmt.Printf("Hasil kuadrat dari %d ^ 2 = %d\n", bilangan, bilangan*bilangan)
+	case bilangan%2 == 0:
+		fmt.Println("Kategori: Bilangan Genap")
+		fmt.Printf("Hasil perkalian dengan bilangan berikutnya %d * %d = %d\n", bilangan, bilangan+1, bilangan*(bilangan+1))
+	case (bilangan-1)%2 == 0:
+		fmt.Println("Kategori: Bilangan Ganjil")
+		fmt.Printf("Hasil penjumlahan dengan bilangan berikutnya %d + %d = %d\n", bilangan, bilangan+1, bilangan+(bilangan+1))
+	}
+}
