1f0d488305be4e26b5cdd3207da91d86abec8b43
Farel Juliyandra Restu Hermawan <fareljuliyandragntg@gmail.com>
2025-11-29 22:43:33 +0700

    Add files via upload

---
+++ FAREL JULIYANDRA RESTU HERMAWAN/modul 11/latsol 3.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+func main() {
+	var bilangan int
+
+	fmt.Print("Masukkan bilangan bulat: ")
+	fmt.Scan(&bilangan)
+
+	var kategori int
+	switch {
+	case bilangan%10 == 0:
+		kategori = 1
+	case bilangan%5 == 0:
+		kategori = 2
+	case bilangan%2 == 1 || bilangan%2 == -1:
+		kategori = 3
+	case bilangan%2 == 0:
+		kategori = 4
+	}
+
+	switch kategori {
+	case 1:
+		hasil := bilangan / 10
+		fmt.Printf("Kategori: Bilangan Kelipatan 10\n")
+		fmt.Printf("Hasil pembagian antara %d / 10 = %d\n", bilangan, hasil)
+	case 2:
+		hasil := bilangan * bilangan
+		fmt.Printf("Kategori: Bilangan Kelipatan 5\n")
+		fmt.Printf("Hasil kuadrat dari %d ^2 = %d\n", bilangan, hasil)
+	case 3:
+		hasil := bilangan + (bilangan + 1)
+		fmt.Printf("Kategori: Bilangan Ganjil\n")
+		fmt.Printf("Hasil penjumlahan dengan bilangan berikutnya %d + %d = %d\n", bilangan, bilangan+1, hasil)
+	case 4:
+		hasil := bilangan * (bilangan + 1)
+		fmt.Printf("Kategori: Bilangan Genap\n")
+		fmt.Printf("Hasil perkalian dengan bilangan berikutnya %d * %d = %d\n", bilangan, bilangan+1, hasil)
+	}
+}
