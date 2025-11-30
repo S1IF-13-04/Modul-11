2d9ddc33add125c901505ba3e98df5a3ed303245
billaforse <anggarabilla@gmail.com>
2025-11-30 21:34:18 +0700

    Create aritmatika.go

---
+++ NAFISAH SALSABILA/aritmatika.go
@@ -0,0 +1,27 @@
+package main
+
+import "fmt"
+
+func main() {
+    var n int
+    fmt.Print("Masukkan bilangan: ")
+    fmt.Scan(&n)
+
+    switch {
+    case n%10 == 0:
+        fmt.Println("Kategori: Bilangan Kelipatan 10")
+        fmt.Println("Hasil pembagian antara", n, "/ 10 =", n/10)
+
+    case n%5 == 0:
+        fmt.Println("Kategori: Bilangan Kelipatan 5")
+        fmt.Println("Hasil kuadrat dari", n, "^2 =", n*n)
+
+    case n%2 == 0:
+        fmt.Println("Kategori: Bilangan Genap")
+        fmt.Println("Hasil perkalian dengan bilangan berikutnya", n, "", n+1, "=", n(n+1))
+
+    default:
+        fmt.Println("Kategori: Bilangan Ganjil")
+        fmt.Println("Hasil penjumlahan dengan bilangan berikutnya", n, "+", n+1, "=", n+(n+1))
+    }
+}
